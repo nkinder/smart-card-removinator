098a7ee953e56e08e6825fa3f641beb599d19867
Scott Poore <spoore@redhat.com>
2021-07-29 16:23:12 -0500

    Add python3.9 and entry_point script
    Adding support for python3.9 and adding console_script for running api
    calls from the command line.

--- client/setup.py
+++ client/setup.py
@@ -9,7 +9,7 @@ with open('README') as f:
 
 setup(
     name='removinator',
-    version='0.1.0.dev3',
+    version='0.1.0.dev4',
     description='A library for controlling the Smart Card Removinator',
     long_description=long_description,
     url='https://github.com/nkinder/smart-card-removinator',
@@ -24,6 +24,11 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=requirements,
+    entry_points="""
+        [console_scripts]
+        rminator=removinator:cli
+    """,
 )
