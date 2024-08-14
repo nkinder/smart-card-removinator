04a7b78514009f9ba56b272a32d3a1457fe8f434
Jiri Jaburek <comps@nomail.dom>
2024-08-14 19:04:42 +0200

    Add lock/unlock commands to the python client
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- client/removinator/__init__.py
+++ client/removinator/__init__.py
@@ -7,6 +7,8 @@ def cli():
     valid_commands = [
         'insert_card',
         'remove_card',
+        'lock_card',
+        'unlock_card',
         'get_status',
     ]
 
@@ -15,6 +17,11 @@ def cli():
 
     parser_insert_card = subparsers.add_parser('insert_card')
     parser_insert_card.add_argument('slot', type=int)
+
+    parser_lock_card = subparsers.add_parser('lock_card')
+    parser_lock_card.add_argument('slot', type=int)
+    parser_unlock_card = subparsers.add_parser('unlock_card')
+    parser_unlock_card.add_argument('slot', type=int)
 
     subparsers.add_parser('remove_card')
     subparsers.add_parser('get_status')
@@ -29,5 +36,11 @@ def cli():
     elif args.command == 'remove_card':
         print('Removing card from current slot')
         conn.remove_card()
+    elif args.command == 'lock_card':
+        print('Locking card slot {}'.format(args.slot))
+        conn.lock_card(args.slot)
+    elif args.command == 'unlock_card':
+        print('Unlocking card slot {}'.format(args.slot))
+        conn.unlock_card(args.slot)
     else:
         print(conn.get_status())
